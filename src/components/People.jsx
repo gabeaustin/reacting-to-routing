f6c3d706a664b5efa8b8e025ff9e0b26d6e452c6
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-02 13:52:26 -0600

    added all people to the people container

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 
 const People = () => {
+    const [people, setPeople] = useState([]);
+
+    useEffect(() => {
+        fetch("https://ghibliapi.herokuapp.com/people")
+        .then(response => response.json())
+        .then(allPeople => console.log(allPeople))
+    }, []);
+
     return (
         <main className="container">
         <section className="row mt-3">
