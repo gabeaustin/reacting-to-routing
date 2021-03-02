5cd673bd0bd28edfdaa77a83adf9de8b8fe18e34
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-02 15:00:38 -0600

    added full film description

--- src/components/FilmDetails.jsx
+++ src/components/FilmDetails.jsx
@@ -17,10 +17,14 @@ import { useState, useEffect } from "react";
         <main className="container">
             <section className="row mt-3">
                 <div className="col-12">
-                    <h4 className="text-center">Details for</h4>
-                    <h1 className="text-center">{film?.title}</h1>
+                    <h4 className="text-center">Film Details for</h4>
+                    <h1 className="text-center">Title: {film?.title}</h1>
 
-                    <h3 className="text-center">{film?.id}</h3>
+                    <h3 className="text-center">Description: {film?.description}</h3>
+                    <h3 className="text-center">Director: {film?.director}</h3>
+                    <h3 className="text-center">Producer: {film?.Producer}</h3>
+                    <h3 className="text-center">Release Date: {film?.release_date}</h3>
+                    <h3 className="text-center">Raw Tomato Score: {film?.rt_score}</h3>
                     {/* <Link to={`/films/${filmid}`} className="btn btn-primary">See Film Details below</Link> */}
 
 
