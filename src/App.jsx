7c322d5dbfbf2487492db73c3fd0ddf528125d6b
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-02 13:09:49 -0600

    added all routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello from App Component!</h1>
-        </div>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+
+                <Route exact path="/films">
+                    <Films />
+                </Route>
+
+                <Route exact path="/people">
+                    <People />
+                </Route>
+
+                <Route path="*">
+                    {() => <h1>404 - Page Not Found</h1>}
+                </Route>
+            </Switch>
+        </BrowserRouter>
     );
 };
 
