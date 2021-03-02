39f5def0f51244a1d101c5ff876e1648ffa8dbc9
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-02 17:06:55 -0600

    moved information into a card and add a back button

--- src/components/FilmDetails.jsx
+++ src/components/FilmDetails.jsx
@@ -17,18 +17,29 @@ import { useState, useEffect } from "react";
         <main className="container">
             <section className="row mt-3">
                 <div className="col-12">
+                    <div class="card" style={{ width: "18rem" }}>
+                        <div class="card-body">
+                            <h5 class="card-title">{film?.title}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                            <p class="card-text">{film?.description}</p>
+                        </div>
+                    </div>
+
+                    <button type="button" class="btn btn-default btn-sm">
+                        <Link to="/films" className="card-link">Go Back</Link>
+                    </button>
+
                     <h4 className="text-center">Film Details for</h4>
                     <h1 className="text-center">Title: {film?.title}</h1>
 
-                    <h3 className="text-center">Description: {film?.description}</h3>
-                    <h3 className="text-center">Director: {film?.director}</h3>
-                    <h3 className="text-center">Producer: {film?.Producer}</h3>
-                    <h3 className="text-center">Release Date: {film?.release_date}</h3>
-                    <h3 className="text-center">Raw Tomato Score: {film?.rt_score}</h3>
+                    <h3 className="text-center">Description: {film?.description}</h3><br/>
+                    <h3 className="text-center">Director: {film?.director}</h3><br/>
+                    <h3 className="text-center">Producer: {film?.Producer}</h3><br/>
+                    <h3 className="text-center">Release Date: {film?.release_date}</h3><br/>
+                    <h3 className="text-center">Raw Tomato Score: {film?.rt_score}</h3><br/>
                     {/* <Link to={`/films/${filmid}`} className="btn btn-primary">See Film Details below</Link> */}
 
 
-                    <Link to="/films" className="btn btn-secondary">Go Back</Link>
 
                 </div>
             </section>
