195b5cf918b0a92b19f25d016d331048634d4aa9
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-02 14:51:42 -0600

    worked on getting all film details to show

--- src/components/FilmDetails.jsx
+++ src/components/FilmDetails.jsx
@@ -1,11 +1,31 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+    
+    const FilmDetails = () => {
+        const { filmid } = useParams();
+        const [film, setFilm] = useState(null); 
+        
+        useEffect(() => {
+            fetch(`https://ghibliapi.herokuapp.com/films/${filmid}`)
+            .then(res => res.json())
+            .then(film => setFilm(film));
+        }, []);
 
-const FilmDetails = () => {
     return (
         <main className="container">
             <section className="row mt-3">
                 <div className="col-12">
-                    <h1 className="text-center">Films</h1>
+                    <h4 className="text-center">Details for</h4>
+                    <h1 className="text-center">{film?.title}</h1>
+
+                    <h3 className="text-center">{film?.id}</h3>
+                    {/* <Link to={`/films/${filmid}`} className="btn btn-primary">See Film Details below</Link> */}
+
+
+                    <Link to="/films" className="btn btn-secondary">Go Back</Link>
+
                 </div>
             </section>
         </main>
--- src/components/Films.jsx
+++ src/components/Films.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 import { useState, useEffect } from "react";
-// import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 const Films = () => {
-    // const { allFilms } = useParams();
     const [films, setFilms] = useState([]);
-
-    // const { filmid } = useParams();
-    // const [film, setFilm] = useState(null);
 
     useEffect(() => {
         fetch("https://ghibliapi.herokuapp.com/films")
             .then(response => response.json())
             .then(films => setFilms(films));
-        // .then(allFilms => console.log(allFilms))
     }, []);
 
     return (
@@ -29,7 +23,7 @@ const Films = () => {
                         <li key={`user-item-${film.id}`}
                             className="list-group-item d-flex justify-content-between align-items-center">
                             {film.title}
-                            <Link to={`/films/${film.id}`} className="btn btn-primary">See Film Info</Link>
+                            <Link to={`/films/${film.id}`} className="btn btn-primary">See Film Details</Link>
                         </li>
                     ))}
                 </ul>
