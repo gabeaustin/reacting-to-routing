1e007732e89498c79ebd67bbf20631b91050073d
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-02 13:48:32 -0600

    added all films to the films container

--- src/components/Films.jsx
+++ src/components/Films.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 
 const Films = () => {
+    const [films, setFilms] = useState([]);
+
+    useEffect(() => {
+        fetch("https://ghibliapi.herokuapp.com/films")
+        .then(response => response.json())
+        .then(allFilms => console.log(allFilms))
+    }, []);
+
     return(
         <main className="container">
         <section className="row mt-3">
             <div className="col-12">
                 <h1 className="text-center">Films</h1>
+                {films.map(film => {
+                    <div className="col-md-6" key={film.id}></div>
+                })}
             </div>
         </section>
     </main>
